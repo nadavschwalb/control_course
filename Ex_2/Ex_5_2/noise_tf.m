e2bae625b72983f363f34da89c2f7d9fc817787e
nadav sholev <nadavschwalb@mail.tau.ac.il>
2020-11-09 17:37:12 +0200

    added EX_2 folder and new functions

---
+++ Ex_2/Ex_5_2/noise_tf.m
@@ -0,0 +1,20 @@
+T_D = 1/(1+L);
+T_D1 = P/(1+L);
+display(T_D);
+display(T_D1);
+
+figure('Name','T_D');
+bodeplot(T_D);
+grid on; grid minor;
+[num,den] = tfdata(T_D);
+syms s
+t_sym = poly2sym(cell2mat(num),s)/poly2sym(cell2mat(den),s);
+latex(t_sym)
+figure('Name','T_D1');
+bodeplot(T_D1);
+grid on; grid minor;
+t_sym = poly2sym(cell2mat(num),s)/poly2sym(cell2mat(den),s);
+latex(t_sym)
+
+ltiview(T_D,T_D1);
+
